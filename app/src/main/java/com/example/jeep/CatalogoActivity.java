1ad9a82138082c4e224c9cac1b47a95d91c619f0
Pablo Fernández <pfalonso96@gmail.com>
2025-11-17 10:35:45 +0100

    Loggin

--- app/src/main/java/com/example/jeep/CatalogoActivity.java
+++ app/src/main/java/com/example/jeep/CatalogoActivity.java
@@ -1,6 +1,5 @@
 package com.example.jeep;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,22 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.jeep.ui.login.LoginActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class CatalogoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        System.out.print("Hello world");
-
-        Intent intento = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intento);
-        finish();
-
+        setContentView(R.layout.activity_catalogo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
